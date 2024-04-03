6944de93c623952353b735aa4a22f715fe65b262
Shubham Sharma <shubhr0813@gmail.com>
2024-04-03 06:57:28 +0530

    Update contact.jsx

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./contact.css";
 // import Phone from "../../img/phone.png";
-import CV from "../../img/ShubhmCV.pdf";
+import CV from "../../img/ShubhamSharma.pdf";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
 import emailjs from "@emailjs/browser";
