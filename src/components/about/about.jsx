7000c3b65c39b81d92272830b2cffdf665dfb3d2
Shubham Sharma <shubhr0813@gmail.com>
2025-04-24 00:37:48 +0530

    Update about.jsx experience year

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -15,7 +15,7 @@ const About = () => {
           <h1 className="a-title">About Me</h1>
           {/* <p className="a-sub">My career so far.</p> */}
           <p className="a-desc">
-           I make websites pretty (and functional, promise!).  Been wrangling pixels and code for over 3.3 years now (yes, I counted the decimal point). 
+           I make websites pretty (and functional, promise!).  Been wrangling pixels and code for over 3.9 years now (yes, I counted the decimal point). 
             I speak fluent ReactJS, HTML, CSS, and JavaScript (and a sprinkle of other fancy acronyms).<br/>
             My superpower? Turning your website vision into a reality that works on any device, from a giant desktop monitor to your grandma's flip phone (okay, maybe not that last one).
             <br/>
